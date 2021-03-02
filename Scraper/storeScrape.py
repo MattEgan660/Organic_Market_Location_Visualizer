5aefc3c8b0c3eeb8251b0634585f53e2674b1194
MattEgan660 <61856548+MattEgan660@users.noreply.github.com>
2021-03-02 18:04:21 -0500

    made scrape tool to get google maps urls from moms locations page

---
+++ Scraper/storeScrape.py
@@ -0,0 +1,49 @@
+# from bs4 import BeautifulSoup
+# import requests
+# import os
+
+
+# def locationScrape(): 
+#     filepath = os.path.join("https://momsorganicmarket.com/all-locations/")
+#     with open(filepath, encoding='utf-8') as file:
+#         html = file.read()
+
+#     soup = BeautifulSoup(html, 'html.parser')
+
+#     locations = soup.find_all('h4', class_='et_pb_module_header')
+
+#     print(locations)
+
+#     for location in locations:
+        
+#         href = location.a['href']
+#         hrefText = href.text
+#         print(hrefText)
+
+
+from splinter import Browser
+from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
+import time 
+
+# Setup splinter
+executable_path = {'executable_path': ChromeDriverManager().install()}
+browser = Browser('chrome', **executable_path, headless=False)
+
+url = 'https://momsorganicmarket.com/all-locations/'
+browser.visit(url)
+
+html = browser.html
+soup = BeautifulSoup(html, 'html.parser')
+
+locations = soup.find_all('h4', class_='et_pb_module_header')
+
+for location in locations: 
+    mapurl = location.a['href']
+    storeLoc = location.text
+    print('-' * 20)
+    print(storeLoc)
+    print(mapurl)
+
+    
+   
