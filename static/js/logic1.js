cc229b92fa44815a2e0a3f75fa0952c84fb5a25b
Dainty Cuevas <73034182+daintycuevas@users.noreply.github.com>
2021-03-13 23:39:21 -0500

    update home.html with img+map

--- static/js/logic1.js
+++ static/js/logic1.js
@@ -1,20 +1,45 @@
-// Use this link to get the geojson data.
-var file = "static/data/organic_markets.json";
+// Define SVG area dimensions
+var svgWidth = 200;
+var svgHeight = 600;
+
+// Define the chart's margins as an object
+var chartMargin = {
+  top: 100,
+  right: 30,
+  bottom: 10,
+  left: 30
+};
+
+// Define dimensions of the chart area
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+// Select body, append SVG area to it, and set the dimensions
+var svg = d3.select("#map")
+  .append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
+
+// Append a group to the SVG area and shift ('translate') it to the right and to the bottom
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
+var organic_file = "/Markets";
+var county_file = "/County";
 
 // Grabbing our GeoJSON data
-d3.json(file, function(data){
-  console.log(data);
+d3.json(organic_file, function(data) {
   // Once we get a response, send the data.features object to the createFeatures function
   createFeatures(data.features);
-  
-})
+});
 
 function createFeatures(organicData) {
 
   // Define a function we want to run once for each feature in the features array
   // Give each feature a popup describing the place and time of the earthquake
   function onEachFeature(feature, layer) {
-    layer.bindPopup("<h3>" + feature.properties.store_name + "</h3><hr><h4> Address: </h4>" + feature.properties.address + "<h4>Rating: </h4>" + feature.properties.rating);
+    layer.bindPopup("<h3>" + feature.properties.store_name + "</h3><hr><h3>" + feature.properties.rating +
+    "</h3><hr><p>" + feature.properties.address + "</p>");
   }
 
   // Create a GeoJSON layer containing the features array on the earthquakeData object
@@ -25,18 +50,16 @@ function createFeatures(organicData) {
 
   // Sending our earthquakes layer to the createMap function
   createMap(organic_markets);
-
 }
 
-function createMap(organic_markets){
-  
+function createMap(organic_markets) {
 
   var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    minZoom: 6,
-    id: "light-v10",
-    accessToken: API_KEY
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  minZoom: 6,
+  id: "light-v10",
+  accessToken: "pk.eyJ1Ijoia2FkYXBlYXJsIiwiYSI6ImNrbGszYmQwajBlMjYyd3FmN21jY3Q0bzkifQ.OaBsr3cGqDPEwhMa1RzitQ"
   })
 
   var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -45,7 +68,7 @@ function createMap(organic_markets){
     maxZoom: 18,
     zoomOffset: -1,
     id: "mapbox/streets-v11",
-    accessToken: API_KEY
+    accessToken: "pk.eyJ1Ijoia2FkYXBlYXJsIiwiYSI6ImNrbGszYmQwajBlMjYyd3FmN21jY3Q0bzkifQ.OaBsr3cGqDPEwhMa1RzitQ"
   });
 
   var baseMaps = {
@@ -58,7 +81,7 @@ function createMap(organic_markets){
   };
 
   var myMap = L.map("map", {
-    center: [39.0458, -76.6413],
+    center: [39.0458, 76.6413],
     zoom: 9,
     layers: [lightmap, organic_markets]
   });
@@ -67,7 +90,4 @@ function createMap(organic_markets){
     collapsed: false
   }).addTo(myMap);
 
-}
-
-
-// [39.3343, -76.4394]
+};
